9821fddcf018b4b7d24dffdbb45d8131071ca41b
KarthikRaja0517 <116252119+KarthikRaja0517@users.noreply.github.com>
2023-09-07 17:15:11 +0530

    Update ConnectionUtil.java

--- src/main/java/com/fssa/connection/ConnectionUtil.java
+++ src/main/java/com/fssa/connection/ConnectionUtil.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.fssa.connection;
 
 import java.sql.*;
@@ -8,7 +7,7 @@ import com.fssa.books.util.CustomLogger;
 public class ConnectionUtil {
 
 	public static Connection getConnection() {
-		Connection con = null;
+		Connection con = null ;
 
 		String url, userName, passWord;
 
@@ -32,44 +31,3 @@ public class ConnectionUtil {
 	}
 
 }
-=======
-package com.fssa.connection;
-
-import java.sql.*;
-
-public class ConnectionUtil {
-
-	public static Connection getConnection() {
-		Connection con = null;
-
-		String url, userName, passWord;
-
-//		Local Credentials
-		url = System.getenv("DATABASE_HOST_LOCAL");
-		userName = System.getenv("DATABASE_USERNAME_LOCAL");
-		passWord = System.getenv("DATABASE_PASSWORD_LOCAL");
-//		url = System.getenv("DATABASE_HOST");
-//		userName = System.getenv("DATABASE_USERNAME");
-//		passWord = System.getenv("DATABASE_PASSWORD");
-		
-
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection(url, userName, passWord);
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
-		return con;
-	}
-
-	public static void main(String[] args) {
-		try {
-			ConnectionUtil.getConnection();
-		} catch (RuntimeException e) {
-			System.out.println(e);
-		}
-	}
-
-}
->>>>>>> 92b7872f20fe8eb92f296630d6ec9bed3b1812a9
